8b84a0cf1b31cef66d0a9f18fd40fe018a605e48
or shtar <orshtar30@gmail.com>
2018-12-24 17:44:49 +0200

    Add project files.

---
+++ victim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace victim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
